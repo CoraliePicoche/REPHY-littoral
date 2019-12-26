bb77576852a1dbd8b117ead0d27de62e5633e1d8
Coralie Picoche <coralie.picoche@u-bordeaux.fr>
2019-12-26 08:15:48 +0100

    More investigation on interaction coefficients for the seed bank paper

--- script/distrib_interaction_coefficients.r
+++ script/distrib_interaction_coefficients.r
@@ -1,19 +1,23 @@
 #2018/09/07 CP
 #We want to have a look at the distributions of interaction coefficients
+#2019/12/26 CP: look at the distribution of coefficients when pooled together
 
 rm(list=ls())
 graphics.off()
 library(MARSS)
 source("script/matrix_MAR_clean.r")
 
-option_model=c("unconstrained","pencen")
+#option_model=c("unconstrained","pencen")
+option_model=c("pencen")
 option_NEI=c("null") #We don't take the "Not Elsewhere Identified" species into account
 groupe=c("BZ","MO","AR","SU")
 option_sp="common" #Species are the same 
 take_0=FALSE #Do we consider the forced 0 in the interaction matrix when computing the mean values of inter-group competition?
 
+all_intra=c()
+all_inter=c()
 for (m in 1:length(option_model)){
-pdf(paste("Rapport/graphe/MAR_estimates/distrib_comp_",option_model[m],"no0.pdf",sep=""))
+pdf(paste("article/graphe/distrib_comp_",option_model[m],"n0.pdf",sep=""))
 for (g in groupe){
         if(g=="BZ"){
                 option_lieu=c("Men er Roue","Loscolo","Croisic")
@@ -31,6 +35,7 @@ for (g in groupe){
 			B=clean_matrix(cis)
 			id_diag=seq(1,dim(B)[1]*dim(B)[2],dim(B)[1]+1)
 			hist(diag(B),xlab="",ylab=option_lieu[ll],main="")
+			all_intra=c(all_intra,diag(B))
 			if(ll==1){
 				mtext("Intra",side=3)
 			}
@@ -42,8 +47,19 @@ for (g in groupe){
 			if(ll==1){
 				mtext("Inter",side=3)
 			}
+			all_inter=c(all_inter,B_nodiag)
+			
 		}
 	}
 dev.off()
 }
 			
+
+pdf(paste("article/graphe/distrib_comp_",option_model[m],"n0_allcoefftogether.pdf",sep=""),width=10,height=6)
+par(mfrow=c(1,2))
+hist(all_intra,xlab="",ylab="",breaks=10,main="Intra")
+hist(all_inter,xlab="",ylab="",breaks=10,main="Inter")
+dev.off()
+
+print(shapiro.test(all_intra))
+print(shapiro.test(all_inter))
--- script/regulation_vs_abundance.r
+++ script/regulation_vs_abundance.r
@@ -1,4 +1,6 @@
 #2018/12/31 CP: is there a link between self-regulation and abundance ?
+#2019/12/26 CP: is there a link between interaction type (pos, neg?) with the others and abundance
+
 rm(list=ls())
 graphics.off()
 library(MARSS)
@@ -14,7 +16,7 @@ name_species=c("AST","CHA","DIT","GUI","LEP","NIT","PLE","PSE","RHI","SKE","THP"
 
 id_lieu=0
 
-tab_tmp=array(NA,dim=c(10,length(name_species),4),dimnames=list(rep("",10),name_species,c("intra","abundance","mean_effect","mean_effect_V"))) #10 subsites, 21 species, 
+tab_tmp=array(NA,dim=c(10,length(name_species),8),dimnames=list(rep("",10),name_species,c("intra","abundance","mean_effect","mean_effect_V","mean_effect_abs","mean_effect_V_abs","sum_pos","sum_pos_V"))) #10 subsites, 21 species, 
 colo=c()
 for (g in groupe){
         if(g=="BZ"){
@@ -48,8 +50,12 @@ for (g in groupe){
 		diag(B_nodiag)=NA
 		for(i in 1:length(sp)){
 	        	tab_tmp[id_lieu,sp[i],"intra"]=B[i,i]
-	        	tab_tmp[id_lieu,sp[i],"mean_effect"]=mean(abs(B_nodiag[B_nodiag[,i]!=0,i]),na.rm=T)
-	        	tab_tmp[id_lieu,sp[i],"mean_effect_V"]=mean(abs(B_nodiag[i,B_nodiag[i,]!=0]),na.rm=T)
+	        	tab_tmp[id_lieu,sp[i],"mean_effect"]=mean(B_nodiag[B_nodiag[,i]!=0,i],na.rm=T)
+	        	tab_tmp[id_lieu,sp[i],"mean_effect_V"]=mean(B_nodiag[i,B_nodiag[i,]!=0],na.rm=T)
+	        	tab_tmp[id_lieu,sp[i],"sum_pos"]=sum(B_nodiag[B_nodiag[,i]!=0,i]>0,na.rm=T)
+	        	tab_tmp[id_lieu,sp[i],"sum_pos_V"]=sum(B_nodiag[i,B_nodiag[i,]!=0]>0,na.rm=T)
+	        	tab_tmp[id_lieu,sp[i],"mean_effect_abs"]=mean(abs(B_nodiag[B_nodiag[,i]!=0,i]),na.rm=T)
+	        	tab_tmp[id_lieu,sp[i],"mean_effect_V_abs"]=mean(abs(B_nodiag[i,B_nodiag[i,]!=0]),na.rm=T)
 	                tab_tmp[id_lieu,sp[i],"abundance"]=mean(tab[,sp[i]],na.rm=T)
 		}
 	}
@@ -57,15 +63,20 @@ for (g in groupe){
 
 pdf("article/graphe/abundance_vs_inter.pdf",height=10)
 par(mfrow=c(3,1),mar=c(4,4,1,1))
-plot(log10(tab_tmp[,,"abundance"]),tab_tmp[,,"mean_effect"],pch=16,xlab="",ylab="Mean abs generality",col=colo,xaxt="n")
+size_c=tab_tmp[,,"sum_pos"]/apply(tab_tmp[,,"sum_pos"],1,max,na.rm=T)
+plot(log10(tab_tmp[,,"abundance"]),tab_tmp[,,"mean_effect"],pch=16,xlab="",ylab="Mean impact",col=colo,xaxt="n",cex=size_c+1)
+legend("topleft",c("Brittany","Oléron","Arcachon","Mediterranean"),col=c("green","darkblue","cyan","darkred"),bty="n",pch=16,cex=1.5)
+abline(h=0,lty=2)
 y1=c(tab_tmp[,,"mean_effect"])
 x1=c(log10(tab_tmp[,,"abundance"]))
 lm1=lm(y1~x1)
 sumlm1=summary(lm1)
 abline(a=lm1$coefficients[1],b=lm1$coefficients[2])
 
-plot(log10(tab_tmp[,,"abundance"]),tab_tmp[,,"mean_effect_V"],pch=16,xlab="",ylab="Mean abs vulnerability",col=colo,xaxt="n")
+size_c=tab_tmp[,,"sum_pos_V"]/apply(tab_tmp[,,"sum_pos_V"],1,max,na.rm=T)
+plot(log10(tab_tmp[,,"abundance"]),tab_tmp[,,"mean_effect_V"],pch=16,xlab="",ylab="Mean vulnerability",col=colo,xaxt="n",cex=size_c+1)
 y1=c(tab_tmp[,,"mean_effect_V"])
+abline(h=0,lty=2)
 lm1=lm(y1~x1)
 sumlm1=summary(lm1)
 abline(a=lm1$coefficients[1],b=lm1$coefficients[2])
@@ -77,6 +88,32 @@ lm1=lm(y1~x1)
 sumlm1=summary(lm1)
 abline(a=lm1$coefficients[1],b=lm1$coefficients[2])
 dev.off()
+
+pdf("article/graphe/abundance_vs_inter_abs_value.pdf",height=10)
+par(mfrow=c(3,1),mar=c(4,4,1,1))
+size_c=tab_tmp[,,"sum_pos"]/apply(tab_tmp[,,"sum_pos"],1,max,na.rm=T)
+plot(log10(tab_tmp[,,"abundance"]),tab_tmp[,,"mean_effect_abs"],pch=16,xlab="",ylab="Mean abs impact",col=colo,xaxt="n",cex=size_c+1)
+y1=c(tab_tmp[,,"mean_effect_abs"])
+x1=c(log10(tab_tmp[,,"abundance"]))
+lm1=lm(y1~x1)
+sumlm1=summary(lm1)
+abline(a=lm1$coefficients[1],b=lm1$coefficients[2])
+
+size_c=tab_tmp[,,"sum_pos_V"]/apply(tab_tmp[,,"sum_pos_V"],1,max,na.rm=T)
+plot(log10(tab_tmp[,,"abundance"]),tab_tmp[,,"mean_effect_V_abs"],pch=16,xlab="",ylab="Mean abs vulnerability",col=colo,xaxt="n",cex=size_c+1)
+y1=c(tab_tmp[,,"mean_effect_V_abs"])
+lm1=lm(y1~x1)
+sumlm1=summary(lm1)
+abline(a=lm1$coefficients[1],b=lm1$coefficients[2])
+
+
+plot(log10(tab_tmp[,,"abundance"]),tab_tmp[,,"intra"],pch=16,ylab="Self regulation",xlab="Log10 abundance",col=colo)
+y1=c(tab_tmp[,,"intra"])
+lm1=lm(y1~x1)
+sumlm1=summary(lm1)
+abline(a=lm1$coefficients[1],b=lm1$coefficients[2])
+dev.off()
+
 
 pdf("article/graphe/abundance_vs_regulation.pdf")
 par(mfrow=c(1,1),mar=c(4.5,4.5,1,1))
