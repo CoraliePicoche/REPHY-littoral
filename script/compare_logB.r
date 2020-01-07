d657e1fe5975eb6d4787c0795fddc82ef8a26e8a
Coralie Picoche <coralie.picoche@u-bordeaux.fr>
2020-01-07 11:54:06 +0100

    Added comparisons between stability indices for B, B-I, log(B)

--- script/compare_logB.r
+++ script/compare_logB.r
@@ -8,6 +8,10 @@ groupe=c("BZ","MO","AR","SU")
 
 #name_species=c("AST","CHA","DIT","GUI","LEP","NIT","PLE","PSE","RHI","SKE","THP","THL","GYM","PRO","PRP","SCR","CRY","EUG")
 
+mat_Bprim=matrix(NA,10,2)
+mat_Blog=matrix(NA,10,2)
+mat_B=matrix(NA,10,2)
+
 id_lieu=0
 
 colo=c()
@@ -44,27 +48,57 @@ for (g in groupe){
 
 		eig_Bprim=eigen(B_prim)$values
 		eig_Blog=eigen(B_log)$values
-		
+		eig_B=eigen(B)$values
+	
+		#Max modulus
+                id=which(Mod(eig_B)==max(Mod(eig_B)))
+                mat_B[id_lieu,1]=Mod(eig_B[id][1])
+                #Max real part
+                id=which(Re(eig_B)==max(Re(eig_B)))
+                mat_B[id_lieu,2]=Re(eig_B[id][1])
+	
 		plot(0,0,t="n",xlim=c(-1,-0.10),ylim=c(-0.25,0.25),xlab="Re",ylab="Im",main=option_lieu[ll])
 		points(Re(eig_Bprim),Im(eig_Bprim),col="black",pch=16,cex=2)
                 #Max modulus
 		id=which(Mod(eig_Bprim)==max(Mod(eig_Bprim)))
                 points(Re(eig_Bprim[id]),Im(eig_Bprim[id]),col="red",cex=2.2,lwd=2)
+		mat_Bprim[id_lieu,1]=Mod(eig_Bprim[id][1])
                 #Max real part
 		id=which(Re(eig_Bprim)==max(Re(eig_Bprim)))
                 points(Re(eig_Bprim[id]),Im(eig_Bprim[id]),col="red",cex=2.2,pch="+")
+		mat_Bprim[id_lieu,2]=Re(eig_Bprim[id][1])
                 
 
 		points(Re(eig_Blog),Im(eig_Blog),col="grey",pch=16,cex=2)
                 #Max modulus
 		id=which(Mod(eig_Blog)==max(Mod(eig_Blog)))
                 points(Re(eig_Blog[id]),Im(eig_Blog[id]),col="orange",cex=2.2,lwd=2)
+		mat_Blog[id_lieu,1]=Mod(eig_Blog[id][1])
                 #Max real part
 		id=which(Re(eig_Blog)==max(Re(eig_Blog)))
                 points(Re(eig_Blog[id]),Im(eig_Blog[id]),col="orange",cex=2.2,pch="+")
+		mat_Blog[id_lieu,2]=Re(eig_Blog[id][1])
 
 		legend("topleft",c("B-I","log(B)"),col=c("black","grey"),bty="n",pch=16,cex=2)
 
 	}
 }
+dev.off()
+
+pdf("article/submit_JEcol/response_R2/maxRe_f_maxMod.pdf")
+plot(0,0,xlim=c(-0.6,-0.1),ylim=c(0.3,1),t="n",xlab="Re(eig)",ylab="Mod(eig)")
+points(mat_Bprim[,2],mat_Bprim[,1],col="black",pch=16)
+points(mat_Blog[,2],mat_Blog[,1],col="grey",pch=16)
+legend("topleft",c("B-I","log(B)"),col=c("black","grey"),bty="n",pch=16,cex=2)
+dev.off()
+
+pdf("article/submit_JEcol/response_R2/eigB_f_eigBprim.pdf")
+par(mfrow=c(1,2))
+plot(0,0,xlim=c(-1,0),ylim=c(0,1),t="n",xlab="B-I or Blog",ylab="B",main="Max Re")
+points(mat_Bprim[,2],mat_B[,2],col="black",pch=16)
+points(mat_Blog[,2],mat_B[,2],col="grey",pch=16)
+legend("topleft",c("B-I","log(B)"),col=c("black","grey"),bty="n",pch=16,cex=2)
+plot(0,0,xlim=c(0,1),ylim=c(0,1),t="n",xlab="B-I or Blog",ylab="B",main="Max Mod")
+points(mat_Bprim[,1],mat_B[,1],col="black",pch=16)
+points(mat_Blog[,1],mat_B[,1],col="grey",pch=16)
 dev.off()
