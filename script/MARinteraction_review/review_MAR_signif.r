1ba917f9bed2bf5f3b90edc71c62551dec00e21f
Coralie Picoche <coralie.picoche@u-bordeaux.fr>
2020-01-29 14:39:02 +0100

    Added explanations for kappa ratio in SI, corrected some sentences in SI, modified Fig. 4, S9 and S10 to remove Barraquand et al. 2018 + corresponding scripts

--- script/MARinteraction_review/review_MAR_signif.r
+++ script/MARinteraction_review/review_MAR_signif.r
@@ -1,5 +1,6 @@
 #CP 12/2017: this script extracts intra/inter ratios from other MAR analyses and other metrics on the matrices and compare them to our results on the REPHY dataset
 #CP 10/2019: identify terrestrial/predation food webs with a different color
+#CP 01/2020: removed Barraquand 2018 as it was redundant with our current data + changed paths to have figures in the response folder + added computation of the mean inter on the same number as intra
 
 graphics.off()
 rm(list=ls())
@@ -9,14 +10,21 @@ source("~/Documents/Plankton/REPHY_littoral/script/matrix_MAR_clean.r")
 whatwewant=function(x_intra,x_inter){
 	dimension=length(x_intra)
 	x_inter=x_inter[x_inter!=0]
+	iter=100
+	mean_ratio_with_sample=0
+	for(i in 1:iter){
+		random_inter=sample(x_inter,dimension,replace=T)
+		mean_ratio_with_sample=mean_ratio_with_sample+(1/iter)*mean(abs(x_intra))/mean(abs(random_inter))
+	}
 	prop_signif=sum(c(x_inter,x_intra)!=0)/(dimension^2)
 	val_signif=c(x_inter,rep(0,dimension*(dimension-1)-length(x_inter)))
-	val=c(mean(abs(x_intra)),sd(abs(x_intra)),mean(abs(x_inter)),sd(abs(x_inter)),mean(abs(val_signif)),sd(abs(val_signif)),dimension,prop_signif)
+	val=c(mean(abs(x_intra)),sd(abs(x_intra)),mean(abs(x_inter)),sd(abs(x_inter)),mean(abs(val_signif)),sd(abs(val_signif)),dimension,prop_signif,mean_ratio_with_sample)
 }
 
 
-tab_answer=matrix(NA,nrow=1,ncol=9)
-colnames(tab_answer)=c("Code","MeanIntra","SdIntra","MeanInter","SdInter","MeanSignif","SdSignif","Dimension","Prop signif")
+tab_answer=matrix(NA,nrow=1,ncol=10)
+colnames(tab_answer)=c("Code","MeanIntra","SdIntra","MeanInter","SdInter","MeanSignif","SdSignif","Dimension","Prop signif","SampledRatio")
+#code_name=c('Hampton2006a_biweekly','Hampton2006a_growing','Hampton2006b_full','Hampton2006b_simple','Griffiths2015_Phyto1','Griffiths2015_Phyto2','Huber2006_Phyto_signif','Huber2006_Ciliate_signif','Ives1999_Zoo1','Ives1999_Zoo2','Ives2003_Plank1','Ives2003_Plank2_signif','Ives2003_Plank3_signif','Klug2000_Phyto','Klug2000_Zoo','Klug2001_TaxoAlgae','Klug2001_MorphoAlgae','Lindegren2009_Fish_signif','Vik2008_LynxHare','Yamamura2006_Insects_signif')
 code_name=c('Hampton2006a_biweekly','Hampton2006a_growing','Hampton2006b_full','Hampton2006b_simple','Griffiths2015_Phyto1','Griffiths2015_Phyto2','Huber2006_Phyto_signif','Huber2006_Ciliate_signif','Ives1999_Zoo1','Ives1999_Zoo2','Ives2003_Plank1','Ives2003_Plank2_signif','Ives2003_Plank3_signif','Klug2000_Phyto','Klug2000_Zoo','Klug2001_TaxoAlgae','Klug2001_MorphoAlgae','Lindegren2009_Fish_signif','Vik2008_LynxHare','Yamamura2006_Insects_signif')
 
 for(c in 1:length(code_name)){
@@ -34,6 +42,7 @@ x_inter=nodiag
 tab_answer=rbind(tab_answer,c(code_name[c],whatwewant(x_intra,x_inter)))
 }
 
+if(1==0){
 #Barraquand2018
 code_name=c(code_name,"Barraquand2018_Teychan","Barraquand2018_B7")
 c=c+1
@@ -57,6 +66,7 @@ nodiag=c(nodiag)
 nodiag=nodiag[!is.na(nodiag)]
 x_inter=nodiag
 tab_answer=rbind(tab_answer,c(code_name[c],whatwewant(x_intra,x_inter)))
+} #Barraquand was index 21/22
 
 tab_answer=tab_answer[-which(is.na(tab_answer[,1])),]
 
@@ -134,7 +144,8 @@ for (gg in 1:length(groupe)){
 tab_answer[,'Prop signif']=1-as.numeric(tab_answer[,'Prop signif']) #Sparsity index
 
 #Comparing
-pdf("~/Documents/Plankton/REPHY_littoral/article/graphe/comparaison_ratio_code_nolog_cleaner_ONLY_SIGNIF.pdf",width=18,height=8)
+#pdf("~/Documents/Plankton/REPHY_littoral/article/graphe/comparaison_ratio_code_nolog_cleaner_ONLY_SIGNIF.pdf",width=18,height=8)
+pdf("~/Documents/Plankton/REPHY_littoral/article/submit_JEcol/response_R2/comparaison_ratio_code_nolog_cleaner_ONLY_SIGNIF_withoutBarraquand_et_al_2018.pdf",width=18,height=8)
 par(mfrow=c(1,2),xpd=NA,mar=c(4,4.5,3,0.5))
 cod_cod=rep("cyan",dim(tab_answer)[1])
 cod_cod[as.numeric(tab_answer[,"Prop signif"])>=0.65]="blue"
@@ -151,16 +162,19 @@ id_diff=16:18
 points(tab_answer[id_diff,"Dimension"],ylab_try[id_diff],col="red",lwd=2,cex=1,pch=symbol[id_diff],bg="red")
 #mtext("Chronological order",side=1,line=2.5,cex=2.0)
 mtext("a)",side=3,cex=1.5,xpd=NA,font=2,line=1.0,adj=0)
-idx=c(1:3,5:21,24:32)
+#idx=c(1:3,5:21,24:32) #w Barraquand
+idx=c(1:3,5:29,22:30)
 text(tab_answer[idx,'Dimension'],ylab_try[idx],names_1[idx],pos=c(4,2),cex=1.5)
 #text(tab_answer[,'Dimension'],ylab_try,names_1,pos=c(4,2),cex=1.5)
 
 idx=4
 text(tab_answer[idx,'Dimension'],ylab_try[idx]+0.05,names_1[idx],pos=c(4),cex=1.5)
 
-idx=22
+#idx=22 #w Barraquand
+idx=20 #wo Barraquand
 text(tab_answer[idx,'Dimension'],ylab_try[idx],names_1[idx],pos=c(4),cex=1.5)
-idx=23
+#idx=23 #w Barraquand
+idx=21 #wo Barraquand
 text(tab_answer[idx,'Dimension'],ylab_try[idx]-0.15,names_1[idx],pos=c(4),cex=1.5)
 
 
@@ -186,24 +200,25 @@ points(xx[id_diff],yy[id_diff],col="red",lwd=2,cex=1,pch=symbol[id_diff],bg="red
 mtext("b)",side=3,cex=1.5,xpd=NA,font=2,line=1.0,adj=0)
 
 names_1_bis=names_1
-names_1_bis[c(2,8,19,28,24,25,31)]=""
+#names_1_bis[c(2,8,19,28,24,25,31)]=""
+names_1_bis[c(2,8,19,26,22,23,29)]=""
 text(xx,yy,names_1_bis,pos=c(4,2),cex=1.5)
 idx=c(2,8)
 text(xx[idx],yy[idx]-0.1,names_1[idx],pos=1,cex=1.5)
 
 text(xx[19],yy[19],names_1[19],pos=2,cex=1.5)
 
-text(xx[24]+0.01,yy[24]+0.1,names_1[24],pos=4,cex=1.5)
-arrows(xx[24]+0.02,yy[24]+0.1,xx[24],yy[24],length=0)
+text(xx[22]+0.01,yy[22]+0.1,names_1[22],pos=4,cex=1.5)
+arrows(xx[22]+0.02,yy[22]+0.1,xx[22],yy[22],length=0)
 
-text(xx[25]-0.02,yy[25]+0.25,names_1[25],pos=2,cex=1.5)
-arrows(xx[25]-0.03,yy[25]+0.25,xx[25],yy[25],length=0)
+text(xx[23]-0.02,yy[23]+0.25,names_1[23],pos=2,cex=1.5)
+arrows(xx[23]-0.03,yy[23]+0.25,xx[23],yy[23],length=0)
 
-text(xx[28]+0.04,yy[28]+0.175,names_1[28],pos=4,cex=1.5)
-arrows(xx[28]+0.05,yy[28]+0.175,xx[28],yy[28],length=0)
+text(xx[26]+0.04,yy[26]+0.175,names_1[26],pos=4,cex=1.5)
+arrows(xx[26]+0.05,yy[26]+0.175,xx[26],yy[26],length=0)
 
-text(xx[31]-0.04,yy[31]-0.025,names_1[31],pos=2,cex=1.5)
-arrows(xx[31]-0.05,yy[31]-0.025,xx[31],yy[31],length=0)
+text(xx[29]-0.04,yy[29]-0.025,names_1[29],pos=2,cex=1.5)
+arrows(xx[29]-0.05,yy[29]-0.025,xx[29],yy[29],length=0)
 
 mtext("Sparsity",side=1,line=2.5,cex=2.0)
 
@@ -224,7 +239,8 @@ mtext("Chronological order",side=1,line=1.5,cex=1.5)
 axis(2,at=c(0,log10(5),1,log10(50),2),lab=c("1","5","10","50","100"),cex.axis=1.5)
 #names_1=tab_answer[,1]
 #text(1:dim(tab_answer)[1],log10(as.numeric(tab_answer[,"MeanIntra"])/as.numeric(tab_answer[,"MeanSignif"]))-.05,names_1)
-id=c(1,2,4:12,14:21)
+#id=c(1,2,4:12,14:21)
+id=c(1,2,4:12,14:20)
 text(xlab_try[id],log10(as.numeric(tab_answer[id,"MeanIntra"])/as.numeric(tab_answer[id,"MeanSignif"])),names_1[id],pos=c(2,4),cex=1.5)
 id=3
 text(xlab_try[id]-0.1,log10(as.numeric(tab_answer[id,"MeanIntra"])/as.numeric(tab_answer[id,"MeanSignif"])),names_1[id],pos=c(2,4),cex=1.5)
@@ -232,13 +248,15 @@ id=13
 text(xlab_try[id],0.05+log10(as.numeric(tab_answer[id,"MeanIntra"])/as.numeric(tab_answer[id,"MeanSignif"])),names_1[id],pos=c(2,4),cex=1.5)
 
 
-text(xlab_try[31:32],log10(as.numeric(tab_answer[31:32,"MeanIntra"])/as.numeric(tab_answer[31:32,"MeanSignif"])),names_1[31:32],pos=c(2,4),cex=1.5)
-idx=c(22,23,29)
+#text(xlab_try[31:32],log10(as.numeric(tab_answer[31:32,"MeanIntra"])/as.numeric(tab_answer[31:32,"MeanSignif"])),names_1[31:32],pos=c(2,4),cex=1.5)
+#idx=c(22,23,29)
+text(xlab_try[29:30],log10(as.numeric(tab_answer[29:30,"MeanIntra"])/as.numeric(tab_answer[29:30,"MeanSignif"])),names_1[29:30],pos=c(2,4),cex=1.5)
+idx=c(25)
 text(xlab_try[idx],log10(as.numeric(tab_answer[idx,"MeanIntra"])/as.numeric(tab_answer[idx,"MeanSignif"])),names_1[idx],pos=c(2,4),cex=1.5)
-idx=30
+idx=28
 text(xlab_try[idx],log10(as.numeric(tab_answer[idx,"MeanIntra"])/as.numeric(tab_answer[idx,"MeanSignif"])),names_1[idx],pos=c(4),cex=1.5)
-id=order(log10(as.numeric(tab_answer[24:28,"MeanIntra"])/as.numeric(tab_answer[24:28,"MeanSignif"])),decreasing=T)
-id=id+23
+id=order(log10(as.numeric(tab_answer[22:26,"MeanIntra"])/as.numeric(tab_answer[22:26,"MeanSignif"])),decreasing=T)
+id=id+21
 for(i in 1:length(id)){
 	ylab_try=log10(as.numeric(tab_answer[id[i],"MeanIntra"])/as.numeric(tab_answer[id[i],"MeanSignif"]))
 	text(xlab_try[id[i]]-0.2,ylab_try-i*0.05,names_1[id[i]],pos=2,cex=1.5)
@@ -250,10 +268,11 @@ for(i in 1:length(id)){
 
 #symbol=rep(21,dim(tab_answer)[1])
 
-pdf("~/Documents/Plankton/REPHY_littoral/article/graphe/sparsity_vs_others_species2taxa.pdf",width=18,height=8)
+#pdf("~/Documents/Plankton/REPHY_littoral/article/graphe/sparsity_vs_others_species2taxa.pdf",width=18,height=8)
+pdf("~/Documents/Plankton/REPHY_littoral/article/submit_JEcol/response_R2/sparsity_vs_others_species2taxa_withoutBarraquand_et_al_2018.pdf",width=18,height=8)
 par(mfrow=c(1,2),xpd=NA,mar=c(4,4.5,3,0.5))
 
-app_T=c(100,100,100,50,300,300,100,100,100,300,200,400,400,300,300,50,100,30,1000,700,300,500)
+app_T=c(100,100,100,50,300,300,100,100,100,300,200,400,400,300,300,50,100,30,1000,700)
 col=rep('black',length(app_T))
 sy=rep(16,length(app_T))
 app_T=c(app_T,app_T_this_study)
@@ -277,7 +296,7 @@ symbol[as.numeric(tab_answer[,"Prop signif"])>=0.65]=22
 symbol[as.numeric(tab_answer[,"Prop signif"])>=0.75]=23
 
 
-pdf('Ratio_function_dim_tmp.pdf',,width=10,height=8)
+pdf('~/Documents/Plankton/REPHY_littoral/article/submit_JEcol/response_R2/Ratio_function_dim_tmp_withoutBarraquand_et_al_2018.pdf',,width=10,height=8)
 par(mfrow=c(1,1),mar=c(4,4.5,1,1))
 plot(tab_answer[,"Dimension"],log10(as.numeric(tab_answer[,"MeanIntra"])/as.numeric(tab_answer[,"MeanSignif"])),t="p",pch=symbol,bg=cod_cod,col="black",cex=2,xlab="Number of taxa",ylab="|intra|/|inter|",yaxt="n",ylim=c(-0.1,2.1),cex.lab=1.5,xlim=c(1.9,14.5),cex.axis=1.5)
 axis(2,at=c(0,log10(5),1,log10(50),2),lab=c("1","5","10","50","100"),cex.axis=1.5)
@@ -285,11 +304,20 @@ id_diff=16:18
 points(tab_answer[id_diff,"Dimension"],log10(as.numeric(tab_answer[id_diff,"MeanIntra"])/as.numeric(tab_answer[id_diff,"MeanSignif"])),col="red",lwd=2,cex=1,pch=symbol[id_diff],bg="red")
 
 
-idx=1:32
+idx=1:30
 tmp=names_1[18]
 names_1[18]=NA
 text(tab_answer[idx,'Dimension'],log10(as.numeric(tab_answer[idx,"MeanIntra"])/as.numeric(tab_answer[idx,"MeanSignif"])),names_1[idx],pos=c(4,2),cex=1.5)
 idx=18
 text(tab_answer[idx,'Dimension'],log10(as.numeric(tab_answer[idx,"MeanIntra"])/as.numeric(tab_answer[idx,"MeanSignif"])),tmp,pos=c(4),cex=1.5)
 legend("bottomright",leg=c('sparsity<0.65',expression("0.65"<="sparsity<0.75"),expression('sparsity'>='0.75')),pt.bg=c("cyan","blue","darkorchid"),pch=c(21,22,23),bty='n',cex=1.5,lty=NA,lwd=1)
+dev.off()
+
+##Comparing mean b_ij with all coefficients and only the same number of coefficients as b_ii
+pdf("Comparison_mean.pdf")
+par(mfrow=c(1,1))
+x=as.numeric(tab_answer[,"MeanIntra"])/as.numeric(tab_answer[,"MeanInter"])
+y=as.numeric(tab_answer[,"SampledRatio"])
+plot(x,y,xlab="All bij",ylab="Sampled bij")
+abline(a=0,b=1)
 dev.off()
